cf3f8aee62ce7298db7cc3a7f46b749c808ee57f
Olena Honcharuk <gasmin07@gmail.com>
2022-12-05 22:44:41 +0200

    feat: add solution for delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
-const remove = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+
+export const remove = async () => {
+
+    const __dirname = path.resolve();
+    const pathFile = path.join(__dirname, 'src/fs/files/fileToRemove.txt');
+
+    fs.access(pathFile, fs.F_OK, (err) => {
+        if (err) return console.log('FS operation failed');
+        fs.unlink(pathFile, () => { });
+    })
 };
 
 await remove();
