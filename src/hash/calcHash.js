343053dd9c182c16b674e27b213682d1743e109e
Olena Honcharuk <gasmin07@gmail.com>
2022-12-05 23:59:15 +0200

    refactor: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -8,7 +8,7 @@ export const calculateHash = async () => {
     // Write your code here 
     const __dirname = dirname(fileURLToPath(import.meta.url));
     const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
-    const fileToCalc = fs.readFile(filePath, 'utf8', (err, data) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
         if (err || !filePath) {
             console.log('FS operation failed');
             throw new Error('FS operation failed');
