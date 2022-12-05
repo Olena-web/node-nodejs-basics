54ba09b3db2bc94016cee8133313761a53c8e9be
Olena Honcharuk <gasmin07@gmail.com>
2022-12-05 23:45:48 +0200

    feat: add solution for wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,14 @@
+import { parentPort, workerData } from 'worker_threads'
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+    const message = {
+        status: 'resolved',
+        data: nthFibonacci(workerData.startNumber),
+    }
+    parentPort.postMessage(message);
     // This function sends result of nthFibonacci computations to main thread
 };
 
