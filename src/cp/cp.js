2591d8209559104de938cd972bd3d8ff921836bb
Olena Honcharuk <gasmin07@gmail.com>
2022-12-05 23:12:56 +0200

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import childProcess from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const spawnChildProcess = async (args) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    console.log(process.argv)
+
+    const child = childProcess.fork(__dirname + '/files/script.js', args);
+
+    process.stdin.on('data', (chunk) => {
+        if (chunk.toString().includes('CLOSE')) process.exit(0);
+        process.stdout.write(`Received from child process: ${chunk.toString()}\n`)
+    });
+
+};
+spawnChildProcess(['args1', 'args2', 'args3', 'args4']);
